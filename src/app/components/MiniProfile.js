3ff77107f88e73f2950e88335e41c2e00635e7ad
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-03-22 19:19:42 -0300

    create mini profile component

---
+++ src/app/components/MiniProfile.js
@@ -0,0 +1,16 @@
+export default function MiniProfile() {
+  return (
+    <div className='flex items-center justify-between mt-14 ml-10'>
+      <img
+        src='https://avatars.githubusercontent.com/u/11988465?v=4'
+        alt='user-image'
+        className='h-16 rounded-full border p-[2px]'
+      />
+      <div className='flex-1 ml-4'>
+        <h2 className='font-bold'>lucas945</h2>
+        <h3 className='text-sm text-gray-400'>Welcome to Instagram</h3>
+      </div>
+      <button className='font-semibold text-blue-400 text-sm'>Sign out</button>
+    </div>
+  );
+}
