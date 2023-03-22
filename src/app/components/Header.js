3eea7ede401574bce2769262f5e1b29434686894
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-03-22 14:24:27 -0300

    add insta logo

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,3 +1,34 @@
+import Image from 'next/image';
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className=''>
+      {/* Left */}
+
+      <div className='flex items-center justify-between max-w-6xl'>
+        <div className='h-24 w-24 relative cursor-pointer hidden lg:inline-grid'>
+          <Image
+            src='http://jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png'
+            alt='Logo'
+            fill
+            className='object-contain'
+          />
+        </div>
+        <div className='h-24 w-10 relative cursor-pointer lg:hidden'>
+          <Image
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png'
+            alt='Logo'
+            fill
+            className='object-contain'
+          />
+        </div>
+
+        <h1>Right side</h1>
+      </div>
+
+      {/* Middle */}
+
+      {/* Right */}
+    </div>
+  );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Header from './components/Header';
 import './globals.css';
 
 export const metadata = {
@@ -10,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body>
-        {/* Header */}
+        <Header />
         {children}
       </body>
     </html>
