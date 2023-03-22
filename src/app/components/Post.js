b265c0a594a9675304d24dedf9b46909e32f1c17
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-03-22 18:11:53 -0300

    add buttons section of the post component

--- src/app/components/Post.js
+++ src/app/components/Post.js
@@ -1,5 +1,11 @@
 import { EllipsisHorizontalIcon } from '@heroicons/react/20/solid';
 
+import {
+  HeartIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+  BookmarkIcon,
+} from '@heroicons/react/24/outline';
+
 export default function Post({ id, userName, userImg, img, caption }) {
   return (
     <div className='bg-white my-7 border rounded-md'>
@@ -16,6 +22,14 @@ export default function Post({ id, userName, userImg, img, caption }) {
 
       {/* Post Image */}
       <img className='object-cover w-full' src={img} alt='' />
+
+      <div className='flex justify-between px-4 pt-4'>
+        <div className='flex space-x-4'>
+          <HeartIcon className='btn' />
+          <ChatBubbleOvalLeftEllipsisIcon className='btn' />
+        </div>
+        <BookmarkIcon className='btn' />
+      </div>
     </div>
   );
 }
