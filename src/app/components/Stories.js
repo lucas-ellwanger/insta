377b53d16e64b3eb77e898d6e1986eed3f197db2
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-03-23 17:00:42 -0300

    modifiy mini profile and story components to include the session

--- src/app/components/Stories.js
+++ src/app/components/Stories.js
@@ -4,9 +4,12 @@ import minifaker from 'minifaker';
 import 'minifaker/locales/en';
 import { useEffect, useState } from 'react';
 import Story from './Story';
+import { useSession } from 'next-auth/react';
 
 export default function Stories() {
+  const { data: session } = useSession();
   const [storyUsers, setStoryUsers] = useState([]);
+
   useEffect(() => {
     const storyUsers = minifaker.array(20, (i) => ({
       userName: minifaker.username({ locale: 'en' }).toLowerCase(),
@@ -18,6 +21,13 @@ export default function Stories() {
 
   return (
     <div className='flex space-x-2 p-6 mx-auto bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-md scrollbar-none'>
+      {session && (
+        <Story
+          userName={session.user.username}
+          img={session.user.image}
+          isUser='true'
+        />
+      )}
       {storyUsers.map((user) => (
         <Story key={user.id} userName={user.userName} img={user.img} />
       ))}
--- src/app/components/Story.js
+++ src/app/components/Story.js
@@ -1,18 +1,20 @@
 import Image from 'next/image';
+import { PlusIcon } from '@heroicons/react/20/solid';
 
-export default function Story({ img, userName }) {
+export default function Story({ img, userName, isUser }) {
   return (
-    <div>
+    <div className='relative group cursor-pointer'>
       <div className='relative h-14 w-14'>
         <Image
-          className='rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out'
+          className='rounded-full p-[1.5px] border-red-500 border-2 hover:scale-110 transition-transform duration-200 ease-out'
           src={img}
           alt=''
           sizes='33vw'
           fill
         />
       </div>
-      <p className='text-xs w-14 truncate'>{userName}</p>
+      {isUser && <PlusIcon className='h-6 absolute top-4 left-4 text-white' />}
+      <p className='text-center text-xs w-14 truncate'>{userName}</p>
     </div>
   );
 }
