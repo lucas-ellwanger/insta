8046eafb9418608a705649afe199dbf9974a9f52
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-03-23 16:35:54 -0300

    check if session exists

--- src/app/components/MiniProfile.js
+++ src/app/components/MiniProfile.js
@@ -10,12 +10,12 @@ export default function MiniProfile() {
       {session ? (
         <div className='flex items-center justify-between mt-14 ml-10'>
           <img
-            src={session?.user.image}
-            alt={session?.user.name}
+            src={session.user.image}
+            alt={session.user.name}
             className='h-16 rounded-full border p-[2px]'
           />
           <div className='flex-1 ml-4'>
-            <h2 className='font-bold'>{session?.user.name}</h2>
+            <h2 className='font-bold'>{session.user.name}</h2>
             <h3 className='text-sm text-gray-400'>Welcome to Instagram</h3>
           </div>
           <button
