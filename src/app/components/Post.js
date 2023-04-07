60d19ed5b69969d98c9fc200ea70c5baa21071cc
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-04-07 11:09:20 -0300

    show the number of likes

--- src/app/components/Post.js
+++ src/app/components/Post.js
@@ -115,6 +115,9 @@ export default function Post({ id, userName, userImg, img, caption }) {
 
       {/* Post Comments */}
       <p className='p-5 truncate'>
+        {likes.length > 0 && (
+          <p className='font-bold mb-1'>{likes.length} likes</p>
+        )}
         <span className='font-bold mr-2'>{userName}</span>
         {caption}
       </p>
