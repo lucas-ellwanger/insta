56cb3f405d077c5843b4dee353dcee184fc2023d
Lucas Ellwanger <lucas_ellwanger@hotmail.com>
2023-04-07 10:33:24 -0300

    apply like functionality

--- src/app/components/Post.js
+++ src/app/components/Post.js
@@ -11,6 +11,9 @@ import {
   query,
   serverTimestamp,
   orderBy,
+  setDoc,
+  doc,
+  deleteDoc,
 } from 'firebase/firestore';
 
 import { EllipsisHorizontalIcon } from '@heroicons/react/20/solid';
@@ -20,11 +23,14 @@ import {
   BookmarkIcon,
   FaceSmileIcon,
 } from '@heroicons/react/24/outline';
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/24/solid';
 
 export default function Post({ id, userName, userImg, img, caption }) {
   const { data: session } = useSession();
   const [comment, setComment] = useState('');
   const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
+  const [hasLiked, setHasLiked] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onSnapshot(
@@ -35,6 +41,29 @@ export default function Post({ id, userName, userImg, img, caption }) {
       }
     );
   }, [db, id]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(db, 'posts', id, 'likes'),
+      (snapshot) => setLikes(snapshot.docs)
+    );
+  }, [db]);
+
+  useEffect(() => {
+    setHasLiked(
+      likes.findIndex((like) => like.id === session?.user.uid) !== -1
+    );
+  }, [likes]);
+
+  async function likePost() {
+    if (hasLiked) {
+      await deleteDoc(doc(db, 'posts', id, 'likes', session.user.uid));
+    } else {
+      await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), {
+        username: session.user.username,
+      });
+    }
+  }
 
   async function sendComment(event) {
     event.preventDefault();
@@ -70,7 +99,14 @@ export default function Post({ id, userName, userImg, img, caption }) {
       {session && (
         <div className='flex justify-between px-4 pt-4'>
           <div className='flex space-x-4'>
-            <HeartIcon className='btn' />
+            {hasLiked ? (
+              <HeartIconFilled
+                onClick={likePost}
+                className='text-red-400 btn'
+              />
+            ) : (
+              <HeartIcon onClick={likePost} className='btn' />
+            )}
             <ChatBubbleOvalLeftEllipsisIcon className='btn' />
           </div>
           <BookmarkIcon className='btn' />
